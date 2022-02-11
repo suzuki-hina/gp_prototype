ac454fca20a19591a391cafeae70b040548493b6
suzuki-hina <ichigodaifuku.0530@gmail.com>
2022-02-11 17:27:01 +0900

    s

---
+++ Assets/Script/SoundPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class SoundPlay : MonoBehaviour
+{
+  private AudioSource button_AudioSource;
+
+  // Use this for initialization
+  void Start()
+  {
+    button_AudioSource = GetComponent<AudioSource>();
+  }
+
+  public void ButtonClick()
+  {
+    button_AudioSource.Play();
+  }
+}
